dcbc7cb187613f22a12756022f9a8807f80f6bba
Patrick Wong <patrickwong38@outlook.com>
2024-01-24 08:21:49 +0000

    Adds apache cookbook

---
+++ recipes/server.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2024, The Authors, All Rights Reserved.
+package 'httpd'
+
+file '/var/www/html/index.html' do
+    content '<h1>Hello, world!<h1>'
+end
+
+service 'httpd' do
+    action [:enable, :start]
+end
